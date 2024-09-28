71e35142e2916f0e3a5985d59c6a357dd34d8bd8
ArAnKon <nastulusaart@gmail.com>
2024-09-28 16:21:52 +0300

    Добавила Жанры и страну производства

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   if (!data) {
     return <span>failed</span>
-  }  
+  }
 
   return (
     <div>
@@ -21,13 +21,30 @@ export default async function Page({ params }: { params: { id: string } }) {
         <div className={styles.serialDetail__content}>
           <div className={styles.serialDetail__info}>
             <span><b>Название</b>: {data.serial_name}</span>
+
             <span><b>Год выпуска</b>: {data.year}</span>
-            <span><b>Актёры</b>: {data.actors_name.join(', ')}</span>
+
+            <div className={styles.info__container}></div>
+            <span><b>Актёры</b>: </span>
+            {data.actors_name.split(',').map((item) => <TagElement key={item} data={item} />)}
+
             <div className={styles.info__container}>
               <span><b>Дабберы</b>: </span>
-              {data.dubers.split(',').map((item) => <TagElement key={item} data={item} />)}
+              {data.dubbers_name.split(',').map((item) => <TagElement key={item} data={item} />)}
             </div>
+
+            <div className={styles.info__container}></div>
+            <span><b>Жанр</b>: </span>
+            {data.genre.split(',').map((item) => <TagElement key={item} data={item} />)}
+
+            <div className={styles.info__container}></div>
+            <span><b>Страна</b>: </span>            
+            {data.country.split(',').map((item) => <TagElement key={item} data={item} />)}
+
+
             <span>{data.description}</span>
+
+
           </div>
           <iframe className={styles.serialDetail__video} src={`//kodik.cc/${data.video}`} width="100%" height="360"
             frameBorder="0" allowFullScreen allow="autoplay *; fullscreen *"></iframe>
