d368e49e7d34945b3b8a013585e87f26137107a6
Gillarion <oiskritsky@gmail.com>
2024-09-29 16:41:17 +0300

    tags fixed

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,54 +1,86 @@
-import Image from "next/image"
-import { Serial } from "@/types";
-import { fetchQuery } from "@/utils/fetchQuery";
-import getResourceURL from "@/utils/getResourceURL";
-import BackButton from "@/components/BackButton/BackButton";
-import styles from "./page.module.css"
-import TagElement from "@/components/TagElement/TagElement";
+import Image from 'next/image';
+import { Serial } from '@/types';
+import { fetchQuery } from '@/utils/fetchQuery';
+import getResourceURL from '@/utils/getResourceURL';
+import BackButton from '@/components/BackButton/BackButton';
+import styles from './page.module.css';
+import TagElement from '@/components/TagElement/TagElement';
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { data }: { data: Serial } = await fetchQuery(`items/serials/${params.id}?fields=*,dubbers_name.dubbers_id.*`)
+  const { data }: { data: Serial } = await fetchQuery(`items/serials/${params.id}?fields=*,dubbers_name.dubbers_id.*`);
 
   if (!data) {
-    return <span>failed</span>
+    return <span>failed</span>;
   }
 
   return (
     <div>
       <BackButton />
       <div className={styles.serialDetail}>
-        <Image className={styles.serialDetail__poster} src={getResourceURL(data.image)} alt={data.serial_name} width={350} height={580} />
+        <Image
+          className={styles.serialDetail__poster}
+          src={getResourceURL(data.image)}
+          alt={data.serial_name}
+          width={350}
+          height={660}
+        />
         <div className={styles.serialDetail__content}>
           <div className={styles.serialDetail__info}>
-            <span><b>Название</b>: {data.serial_name}</span>
+            <span>
+              <b>Название</b>: {data.serial_name}
+            </span>
 
-            <span><b>Год выпуска</b>: {data.year}</span>
-            <div className={styles.info__container}></div>
-            <span><b>Актёры</b>: </span>
-            {data.actors_name.split(',').map((item) => <TagElement key={item} data={item} />)}
-
+            <span>
+              <b>Год выпуска</b>: {data.year}
+            </span>
             <div className={styles.info__container}>
-              <span><b>Дабберы</b>: </span>
-              {data.dubbers_name.split(',').map((item) => <TagElement key={item} data={item} />)}
+              <span>
+                <b>Актёры</b>:{' '}
+              </span>
+              {data.actors_name.split(',').map((item) => (
+                <TagElement key={item} data={item} />
+              ))}
             </div>
 
-            <div className={styles.info__container}></div>
-            <span><b>Жанр</b>: </span>
-            {data.genre.split(',').map((item) => <TagElement key={item} data={item} />)}
+            <div className={styles.info__container}>
+              <span>
+                <b>Дабберы</b>:{' '}
+              </span>
+              {data.dubbers_name.split(',').map((item) => (
+                <TagElement key={item} data={item} />
+              ))}
+            </div>
 
-            <div className={styles.info__container}></div>
-            <span><b>Страна</b>: </span>            
-            {data.country.split(', ').map((item) => <TagElement key={item} data={item} />)}
+            <div className={styles.info__container}>
+              <span>
+                <b>Жанр</b>:{' '}
+              </span>
+              {data.genre.split(',').map((item) => (
+                <TagElement key={item} data={item} />
+              ))}
+            </div>
 
+            <div className={styles.info__container}>
+              <span>
+                <b>Страна</b>:{' '}
+              </span>
+              {data.country.split(', ').map((item) => (
+                <TagElement key={item} data={item} />
+              ))}
+            </div>
 
             <span>{data.description}</span>
-
-
           </div>
-          <iframe className={styles.serialDetail__video} src={`//kodik.cc/${data.video}`} width="100%" height="360"
-            frameBorder="0" allowFullScreen allow="autoplay *; fullscreen *"></iframe>
+          <iframe
+            className={styles.serialDetail__video}
+            src={`//kodik.cc/${data.video}`}
+            width='100%'
+            height='360'
+            frameBorder='0'
+            allowFullScreen
+            allow='autoplay *; fullscreen *'></iframe>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,41 @@
-import type { Metadata } from "next";
-import Image from "next/image"
-import Link from "next/link"
-import { Montserrat } from "next/font/google";
-import "./globals.css";
-import styles from "@/app/page.module.css";
-import FilmButton from "@/components/FilmButton/FilmButton";
-import SerialButton from "@/components/SerialButton/SerialButton";
+import type { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Montserrat } from 'next/font/google';
+import './globals.css';
+import styles from '@/app/page.module.css';
+import FilmButton from '@/components/FilmButton/FilmButton';
+import SerialButton from '@/components/SerialButton/SerialButton';
 
 const montserrat = Montserrat({
-  subsets: ["latin", "cyrillic"],
-  weight: ["400", "700"],
-  display: "swap",
-})
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '700'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
-  title: "MK Studio",
-  description: "Сайт с корейскими дорамами",
+  title: 'MK Studio',
+  description: 'Сайт с корейскими дорамами',
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru" className={montserrat.className}>
-    <body className={styles.body}>
-    <Link href="/">
-      <Image src="/logo2.png" alt="MK Studio" width={300} height={90}/>
-    </Link>
-    <FilmButton /> <SerialButton /> 
-    <main className={styles.main}>
-      {children}
-    </main>
-    </body>
+    <html lang='ru' className={montserrat.className}>
+      <body>
+        <div className={styles.logo}>
+          <Link href='/'>
+            <Image src='/logo2.png' alt='MK Studio' width={300} height={90} />
+          </Link>
+        </div>
+        <span className={styles.border}></span>
+        <FilmButton />
+
+        <main className={styles.main}>{children}</main>
+      </body>
     </html>
   );
 }
